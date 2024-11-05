32ba896772389c50541b89f61095ea5da619e614
Mehulyadavv <mehulyadav432@gmail.com>
2024-11-05 17:41:42 +0530

    2nd commit

---
+++ todo_app/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const {UserModel, TodoModel} = require("./db");
+const app = express();
+app.use(express.json());
+
+app.post("signup", async function(req, res){
+
+    
+  
+
+});
+
+app.post("signin", function(req, res){
+
+    
+});
+
+app.post("todo", function(req, res){
+
+    
+});
+
+app.post("todos", function(req, res){
+
+    
+});
